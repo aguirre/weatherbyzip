079b5a370c8ae28b4e9173ede048bbcae932b81a
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-10-04 12:21:16 -0700

    change to daily api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,24 +5,33 @@ $("#zipSearch").on("click", function(e) {
     .trim();
   var APIKey = "166a433c57516f51dfab1f7edaed8413";
   var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?zip=" +
+    "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" +
     zipcode +
-    ",us&units=imperial&appid=" +
+    ",us&units=imperial&cnt=16&appid=" +
     APIKey;
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    $(".city").html("<h1>" + response.city.name + " Weather Details</h1>");
     $(".icon").html(
       "<img src='http://openweathermap.org/img/w/" +
-        response.weather[0].icon +
+        response.list[0].weather[0].icon +
         ".png'>"
     );
-    $(".weather").text("Weather: " + response.weather[0].description);
-    $(".temp").text("Temperature (F) " + response.main.temp);
-    $(".humidity").text("Humidity: " + response.main.humidity);
-    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".weather").text("Weather: " + response.list[0].weather[0].description);
+    $(".temp").text(
+      "Temperature: High " +
+        response.list[0].temp.max +
+        " F | Low " +
+        response.list[0].temp.min +
+        " F"
+    );
+    $(".humidity").text("Humidity: " + response.list[0].humidity);
+    $(".wind").text("Wind Speed: " + response.list[0].speed);
   });
 });
+
+// make 4 loop for days
+// days have icon / temp hi and low / description / wind
