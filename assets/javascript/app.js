ded7bb7dea37a38558ef2ed0bb4f79b84f78bc79
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-10-05 18:12:51 -0700

    add tempstap convert

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,28 @@ $("#weatherSearch").on("click", function(e) {
     .val()
     .trim();
 
-  // API For Weather Info by ZIP Code
+  // API to get City and State by ZIP Code
+  var zipURL = "https://api.zippopotam.us/us/" + weatherZip;
+  $.ajax({
+    url: zipURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    $(".city").html(
+      "<h1>" +
+        response.places[0]["place name"] +
+        ", " +
+        response.places[0]["state abbreviation"] +
+        "</h1>"
+    );
+  });
+
+  // API for Weather Info by ZIP Code
   var APIKey = "166a433c57516f51dfab1f7edaed8413";
   var weatherURL =
     "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" +
     weatherZip +
-    ",us&units=imperial&cnt=16&appid=" +
+    ",us&units=imperial&cnt=7&appid=" +
     APIKey;
   $.ajax({
     url: weatherURL,
@@ -21,24 +37,33 @@ $("#weatherSearch").on("click", function(e) {
         response.list[0].weather[0].icon +
         ".png'>"
     );
-    $(".weather").text("Weather: " + response.list[0].weather[0].description);
-    $(".temp-hi").text("High: " + response.list[0].temp.max + " F");
-    $(".temp-lo").text("Low: " + response.list[0].temp.min + " F");
-    $(".humidity").text("Humidity: " + response.list[0].humidity);
-    $(".wind").text("Wind Speed: " + response.list[0].speed);
-  });
-
-  // API to get City name by ZIP Code
-  var zipURL = "https://api.zippopotam.us/us/" + weatherZip;
-  $.ajax({
-    url: zipURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    $(".city").html(
-      "<h1>" + response.places[0]["place name"] + " Weather Details</h1>"
+    $(".weather").text(response.list[0].weather[0].description);
+    $(".temp-hi").text(
+      "High: " + Math.round(response.list[0].temp.max) + "° F"
     );
+    $(".temp-lo").text("Low: " + Math.round(response.list[0].temp.min) + "° F");
+    $(".wind").text("Wind: " + response.list[0].speed + " mph");
+
+    // Converts Unix Timestamp to Date
+    var unixConvert = moment.unix(response.list[0].dt).format("dddd, MMM D");
+    console.log(unixConvert);
+    $(".weatherDate").html("<h3>" + unixConvert + "</h3>");
   });
 });
 
-// make 4 loop for days
+//! make 4 loop for days
+// for (var i = 0; i < response.list[i].length; i++) {
+//   var newRow = $("<tr>").append(
+//     $("<td>").html("<h3>" + unixConvert + "</h3>"),
+//     $("<td>").html(
+//       "<img src='https://openweathermap.org/img/w/" +
+//         response.list[0].weather[0].icon +
+//         ".png'>"
+//     ),
+//     $("<td>").text(response.list[0].weather[0].description),
+//     $("<td>").text("High: " + Math.round(response.list[0].temp.max) + "° F"),
+//     $("<td>").text("Low: " + Math.round(response.list[0].temp.min) + "° F"),
+//     $("<td>").text("Wind: " + response.list[0].speed + " mph")
+//   );
+//   $("#weatherRow > tbody").append(newRow);
+// }
